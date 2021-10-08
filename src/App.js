1d6a0254f5f31d312f3a6495ad79e772ef5e2d5c
Robert Stetson <88099719+robstetson@users.noreply.github.com>
2021-10-08 01:00:24 -0400

    Revert "Saving for the day, got all Files and folders working correctly."

--- src/App.js
+++ src/App.js
@@ -1,84 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import axios from 'axios';
-import TitleFunc from "./Nasa/NasaTop";
-import MidSect from "./Nasa/NasaMid";
-import DescSect from "./Nasa/NasaThird";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Spinner } from "reactstrap";
-import styled from 'styled-components';
-
-
-
-
-
-// Very large rainbow spinner section
-export const RainbowSpinner = (props) => {
-  return(
-    <div>
-      <Spinner type='grow' color='red' children= '🔥'/>
-      <Spinner type='grow' color='secondary' children= '❄'/>
-      <Spinner type='grow' color='success' children= '🔥'/>
-      <Spinner type='grow' color='danger' children= '❄'/>
-      <Spinner type='grow' color='warning' children= '🔥'/>
-      <Spinner type='grow' color='info' children= '❄'/>
-      <Spinner type='grow' color='light' children= '🔥'/>
-      <Spinner type='grow' color='dark' children= '❄'/>
-      <Spinner type='grow' color='red' children= '🔥'/>
-      <Spinner type='grow' color='secondary' children= '❄'/>
-      <Spinner type='grow' color='success' children= '🔥'/>
-      <Spinner type='grow' color='danger' children= '❄'/>
-      <Spinner type='grow' color='warning' children= '🔥'/>
-      <Spinner type='grow' color='info' children= '❄'/>
-      <Spinner type='grow' color='light' children= '🔥'/>
-      <Spinner type='grow' color='dark' children= '❄'/>
-      <Spinner type='grow' color='red' children= '🔥'/>
-      <Spinner type='grow' color='secondary' children= '❄'/>
-      <Spinner type='grow' color='success' children= '🔥'/>
-      <Spinner type='grow' color='danger'children= '❄'/>
-      <Spinner type='grow' color='warning' children='🔥'/>
-      <Spinner type='grow' color='info' children= '❄'/>
-      <Spinner type='grow' color='light' children= '🔥'/>
-      <Spinner type='grow' color='dark' children= '❄'/>
-      <Spinner type='grow' color='red' children= '🔥'/>
-      <Spinner type='grow' color='secondary' children= '❄'/>
-      <Spinner type='grow' color='success' children= '🔥'/>
-      <Spinner type='grow' color='danger' children= '❄'/>
-      <Spinner type='grow' color='warning' children= '🔥'/>
-      <Spinner type='grow' color='info' children= '❄'/>
-      <Spinner type='grow' color='light' children= '🔥'/>
-      <Spinner type='grow' color='dark' children= '❄'/>
-      <Spinner type='grow' color='red' children= '🔥'/>
-      <Spinner type='grow' color='secondary' children= '❄'/>
-      <Spinner type='grow' color='success' children= '🔥'/>
-      <Spinner type='grow' color='danger' children= '❄'/>
-      <Spinner type='grow' color='warning'children= '🔥' />
-      <Spinner type='grow' color='info' children= '❄'/>
-   
-    </div>
-  )
-}
-
-const Body = styled.body`
-background-color: black;
-color: #BB8FCE;
-`
-
 
 function App() {
-
-
- 
   return (
     <div className="App">
-
-<Body>
-<TitleFunc />
-<MidSect />
-<DescSect />
-
-<RainbowSpinner />
-</Body>
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+      </p>
     </div>
   );
 }
